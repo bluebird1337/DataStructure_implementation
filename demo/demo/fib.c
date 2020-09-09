a4f529beef9d4de631f7a9a8bf1f6e762acdd8db
Wang Hong Wen <whwen0528@gmail.com>
2020-09-09 22:11:43 +0800

    fibonacci added

---
+++ demo/demo/fib.c
@@ -0,0 +1,33 @@
+#include<stdio.h>
+
+int fib(int x) {
+	if (x == 1 || x == 2)
+		return 1;
+	else if (x == 0)
+		return 0;
+	else
+		return fib(x - 1) + fib(x - 2);
+}
+
+int fib_loop(int x) {
+	int t0 = 0, t1 = 1, r;
+	for (int i = 2; i <= x; i++) {
+		r = t0 + t1;
+		t0 = t1;
+		t1 = r;
+	}
+	return r;
+}
+
+int data[100];
+
+int fib_store(int n) {
+	if (data[n] < 0) {
+		int temp = fib_store(n - 1) + fib_store(n - 2);
+		data[n] = temp;
+		return temp;
+	}
+	else {
+		return data[n];
+	}
+}
--- demo/demo/main.c
+++ demo/demo/main.c
@@ -5,10 +5,18 @@ void head(int x);
 void tail(int x);
 int f(int n);
 double taylor_horner(int x, int n);
+int fib(int x);
+int fib_loop(int x);
+int fib_store(int n);
 
+int data[100];
 int main() {
-	printf("%lf \n", taylor_horner(4, 15) );
+	data[0] = 0;
+	data[1] = 1;
+	for (int i = 2; i < 100; i++)
+		data[i] = -1;
 
+	printf("%d \n", fib_store(7) );
 	return 0;
 }
 
